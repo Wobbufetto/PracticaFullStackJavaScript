3b9f47e4b3605f60ed43c278b1433a5ebfa76056
Juan Camilo Morales Sanchez <juanka970129@gmail.com>
2024-06-06 17:42:24 -0500

    Creación de pagina personal, css y jQuery

---
+++ public/imports.js
@@ -0,0 +1,21 @@
+// public/main.js
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'font-awesome/css/font-awesome.min.css';
+import 'animate.css/animate.min.css';
+import 'ionicons/dist/css/ionicons.min.css';
+import 'owl.carousel/dist/assets/owl.carousel.min.css';
+import 'lightbox2/dist/css/lightbox.min.css';
+
+import 'jquery';
+import 'jquery-migrate';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import 'easing';
+import 'superfish';
+import 'wowjs/dist/wow.min.js';
+import 'waypoints/lib/noframework.waypoints.js';
+import 'counterup/jquery.counterup.min.js';
+import 'owl.carousel';
+import 'isotope-layout';
+import 'lightbox2';
+import 'jquery-touchswipe';
